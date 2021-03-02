ba3d2063dae8b8774c6fde1dc347cd7ca1f7a298
guilhae <guilha@drena.xyz>
2021-03-02 11:34:53 +0000

    Página inicial com ultimos vídeos

--- index.php
+++ index.php
@@ -18,6 +18,41 @@ require('head.php');
                 <h1 class="display-3 text-outline">MOSTRA O QUE FAZES.<br>
 				<!--<svg class='bi' width='1em' height='1em' fill='currentColor'><use xlink:href='node_modules/bootstrap-icons/bootstrap-icons.svg#play'/></svg>--></h1>
             </div>
+			<?php
+            echo "
+            <div class='p-0 my-0 my-xl-4 col-xl-6 offset-xl-3 text-center'>
+				<h1 class='display-3 m-5'>ULTIMOS VÍDEOS</h1>
+                <div class='row row-cols-3 row-cols-md-4'>
+                ";
+                function encurtarNome($nome){
+                    if (strlen($nome)>=30){
+                        return (substr($nome, 0, 28)."…");
+                    } else {
+                        return ($nome);
+                    }
+                }
+                $pesquisa = "SELECT * FROM med ORDER by den DESC LIMIT 12";
+                if ($resultado = $bd->query($pesquisa)) {
+                    while ($campo = $resultado->fetch_assoc()) {
+                        echo "
+                        <div class='col mb-4'>
+                            <div class='card border-0 shadow'>
+                                <a href='/video?id=".$campo['id']."'>
+                                    <img src='http://media.drena.xyz/thumb/".$campo['id'].".jpg' class='card-img-top' alt='...'>
+                                </a>
+                                <div class='card-body bg-dark text-left'>
+                                    <p class='text-light card-text'>".encurtarNome($campo['nom'])."</p>
+                                </div>
+                            </div>
+                        </div>
+                        ";
+                    } 
+                    $resultado->free();
+                }
+                echo "
+                </div>
+            </div>";
+			?>
         </div>
 	</body>
 </html>
--- teste.php
+++ teste.php
@@ -1,33 +1,14 @@
-<?php 
-require('head.php');
-$ultimo_video = mysqli_fetch_assoc(mysqli_query($bd, "SELECT * FROM med ORDER BY den DESC LIMIT 1"));
-header("Location: /video?id=".$ultimo_video['id']);
+<style>
+body{
+	font-family: sans-serif;
+}
+</style>
+<?php
+
+for ($j = 1; $j <= 25; $j+=0.5) {
+
+	echo "<b>".$j."</b>:<br>".(sqrt(2)*$j)."<br>";
+
+}
+
 ?>
-	</head>
-	<body>
-		<?php require('cabeçalho.php'); ?>
-		<div id="swup" class="transition-fade">
-		<!--<div class='text-dark p-5 my-4 col-xl-6 offset-xl-3'>
-			<div class='card-deck'>
-				<div class="card">
-					<img src="..." class="card-img-top" alt="...">
-						<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-					</div>
-				</div>
-				<div class="card">
-					<img src="..." class="card-img-top" alt="...">
-						<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-					</div>
-				</div>
-			</div>
-		</div>-->
-		
-	</div>
-	</body>
-</html>
