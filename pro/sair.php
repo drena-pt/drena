8f7543f26b9084648be5e4344155b6cd888a969e
guilhae <guilha@drena.xyz>
2023-03-23 01:21:39 +0000

    Ao terminar sessão o cookie token desaparece

--- pro/sair.php
+++ pro/sair.php
@@ -1,8 +1,9 @@
 <?php
 #Processo - Terminar sessão
+include_once('fun_var.php');#Variáveis
 session_start();
 session_destroy();
 header("Location: ".$_SERVER['HTTP_REFERER']);
-setcookie('drena_token', '', time()-100, '/');
+setcookie('drena_token', '', time()-100, '/', '.'.$url_dominio);
 exit;
 ?>
