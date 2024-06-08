ab735a53d70ba57cc7e03d94e2d3d89bcb0ab4ad
guilhae <guilha@drena.pt>
2024-06-08 12:32:10 +0100

    Ferramenta de espaço melhor

--- mod_espaco.php
+++ mod_espaco.php
@@ -39,14 +39,13 @@
                 return $size;
             }
 
-            $pastas = array("img","ori","comp","conv","fpe","som","thumb");
+            $pastas = array("ori","comp","conv","img","som","fpe","thumb");
             $pastas_tam = array();
-
-            $tam_total = folderSize($dir_media);
-
             foreach ($pastas as $index => $value) {
                 $pastas_tam[$value] = folderSize($dir_media.$value);
             }
+            $tam_total = array_sum($pastas_tam);
+
             asort($pastas_tam);
 
             #Espaço
@@ -65,7 +64,125 @@
                         </div>
                         ";
                     }
-                    echo "<br><tt class='d-flex justify-content-between'>"._('Total')."<small>".bytesParaHumano($tam_total)."</small></tt>
+                    
+                    $tipo_pasta = [];
+                    $tipo_pasta['vid1'] = $pastas_tam['ori'];
+                    $tipo_pasta['vid2'] = ($pastas_tam['comp']+$pastas_tam['conv']);
+                    $tipo_pasta['img1'] = $pastas_tam['img'];
+                    $tipo_pasta['img2'] = ($pastas_tam['fpe']+$pastas_tam['thu']);
+                    $tipo_pasta['som'] = $pastas_tam['som'];
+                    $tipo_pasta_total = array_sum($tipo_pasta);
+
+                    echo "<br><tt class='d-flex justify-content-between'>"._('Total')."<small>".bytesParaHumano($tam_total)."</small></tt>";
+
+                    echo "
+                    <br>
+                    <!--<small>"._("Original")."</small>-->
+					<span class='me-3 text-primary'><i class='bi bi-camera-video'></i>".round(($tipo_pasta['vid1'] / $tipo_pasta_total) * 100, 2)."%</span>
+                    <span class='me-3 text-ciano'><i class='bi bi-image'></i>".round(($tipo_pasta['img1'] / $tipo_pasta_total) * 100, 2)."%</span>
+					<span class='me-3 text-rosa'><i class='bi bi-soundwave'></i>".round(($tipo_pasta['som'] / $tipo_pasta_total) * 100, 2)."%</span>
+                    <div class='progress'>";
+
+                    foreach ($tipo_pasta as $pastas_nome => $tam) {
+                        switch ($pastas_nome) {
+                            case 'vid1':
+                              $progress_color = "primary";
+                              break;
+                            case 'vid2':
+                              $progress_color = "primary bg-opacity-50";
+                              break;
+                            case 'img1':
+                              $progress_color = 'ciano';
+                              break;
+                            case 'img2':
+                              $progress_color = 'ciano bg-opacity-50';
+                              break;
+                            case 'som':
+                              $progress_color = "rosa";
+                              break;
+                        }
+
+                        $percentage = round(($tam / $tipo_pasta_total) * 100, 2);
+                        echo "<div class='progress-bar bg-".$progress_color."' role='progressbar' style='width: ".$percentage."%'></div>";
+                    }
+                    echo "
+                    </div>
+                    <!--<small>"._("Processado")."</small>-->
+					<span class='me-3 text-primary'><i class='bi bi-play-btn'></i>".round(($tipo_pasta['vid2'] / $tipo_pasta_total) * 100, 2)."%</span>
+                    <span class='me-3 text-ciano'><i class='bi bi-person-video'></i>".round(($tipo_pasta['img2'] / $tipo_pasta_total) * 100, 2)."%</span>
+                    ";
+
+
+
+                    
+                    /*
+
+                    //Pastas de ficheiros carregados originais
+                    $ori_pastas = [];
+                    $ori_pastas['vid'] = $pastas_tam['ori'];
+                    $ori_pastas['img'] = $pastas_tam['img'];
+                    $ori_pastas['som'] = $pastas_tam['som'];
+                    $ori_pastas_total = array_sum($ori_pastas);
+
+                    echo "
+                    <br><br><small>"._("Original")."</small><br>
+					<span class='me-3 text-primary'><i class='bi bi-camera-video'></i>".round(($ori_pastas['vid'] / $ori_pastas_total) * 100, 2)."%</span>
+                    <span class='me-3 text-ciano'><i class='bi bi-image'></i>".round(($ori_pastas['img'] / $ori_pastas_total) * 100, 2)."%</span>
+					<span class='me-3 text-rosa'><i class='bi bi-soundwave'></i>".round(($ori_pastas['som'] / $ori_pastas_total) * 100, 2)."%</span>
+                    <div class='progress'>";
+                        foreach ($ori_pastas as $nom => $tam) {
+                            switch ($nom) {
+                                case 'vid':
+                                $progress_color = "primary";
+                                break;
+                                case 'img':
+                                $progress_color = 'ciano';
+                                break;
+                                case 'som':
+                                $progress_color = "rosa";
+                                break;
+                            }
+                            $percentage = round(($tam / $ori_pastas_total) * 100, 2);
+                            echo "<div class='progress-bar bg-".$progress_color."' role='progressbar' style='width: ".$percentage."%'></div>";
+                        }
+                    echo "</div>";
+                    ////////////////////////////////
+
+                    
+                    //Pastas de ficheiros processados
+                    $pro_pastas = [];
+                    $pro_pastas['vid'] = ($pastas_tam['comp']+$pastas_tam['conv']);
+                    $pro_pastas['thu'] = $pastas_tam['thumb'];
+                    $pro_pastas['fpe'] = $pastas_tam['fpe'];
+                    $pro_pastas_total = array_sum($pro_pastas);
+
+                    echo "
+                    <br><small>"._("Processado")."</small><br>
+					<span class='me-3 text-primary'><i class='bi bi-camera-video'></i>".round(($pro_pastas['vid'] / $pro_pastas_total) * 100, 2)."%</span>
+                    <span class='me-3 text-ciano'><i class='bi bi-image'></i>".round(($pro_pastas['thu'] / $pro_pastas_total) * 100, 2)."%</span>
+					<span class='me-3 text-rosa'><i class='bi bi-soundwave'></i>".round(($pro_pastas['fpe'] / $pro_pastas_total) * 100, 2)."%</span>
+                    <div class='progress'>";
+                        foreach ($pro_pastas as $nom => $tam) {
+                            switch ($nom) {
+                                case 'vid':
+                                $progress_color = "primary bg-opacity-50";
+                                break;
+                                case 'thu':
+                                $progress_color = 'ciano';
+                                break;
+                                case 'fpe':
+                                $progress_color = "ciano bg-opacity-50";
+                                break;
+                            }
+                            $percentage = round(($tam / $pro_pastas_total) * 100, 2);
+                            echo "<div class='progress-bar bg-".$progress_color."' role='progressbar' style='width: ".$percentage."%'></div>";
+                        }
+                    echo "</div>";
+                    ////////////////////////////////
+
+                    */
+
+                    echo "
 
                 </section>
 
